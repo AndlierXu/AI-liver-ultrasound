9363030fceb47e28118aaf715489e88d5a9abed6
AndlierXu <33342392+AndlierXu@users.noreply.github.com>
2022-11-03 12:55:23 +0800

    Update lm_net.py

--- model/model/lm_net.py
+++ model/model/lm_net.py
@@ -6,7 +6,6 @@ class lm_net(nn.Module):
     def __init__(self, config):
         super(lm_net, self).__init__()
         self.densenet = models.densenet121(pretrained=True)
-        self.densenet.features.conv0 = nn.Conv2d(6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         self.densenet.classifier = nn.Linear(1024, 2)
 
     def forward(self, data, accu_result = None):
